152ebf07bed51dafa1333f901f6519b97ee210ab
Bart Frackiewicz <decomplosed@gmail.com>
2020-07-04 16:04:50 +0200

    added image of the character

--- src/components/characters/CharacterItem.js
+++ src/components/characters/CharacterItem.js
@@ -1,10 +1,13 @@
 import React from 'react'
 
 const CharacterItem = ({ item }) => {
+  console.log(item)
   return (
-    <div className="card">
-      <div className="card-inner">
-        
+    <div className='card'>
+      <div className='card-inner'>
+        <div className='card-front'>
+          <img src={item.img} alt={item.name} />
+        </div>
       </div>
     </div>
   )
