25a0b3310e669d330e63e885eacfd6e95dc983b7
Bart Frackiewicz <decomplosed@gmail.com>
2020-07-04 16:07:06 +0200

    added status data

--- src/components/characters/CharacterItem.js
+++ src/components/characters/CharacterItem.js
@@ -19,7 +19,9 @@ const CharacterItem = ({ item }) => {
             <li>
               <strong>Birthday:</strong> {item.birthday}
             </li>
-            <li></li>
+            <li>
+              <strong>Status:</strong> {item.status}
+            </li>
           </ul>
         </div>
       </div>
