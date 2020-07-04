980c440df16740e7363a78dd34cd78c0b8766072
Bart Frackiewicz <decomplosed@gmail.com>
2020-07-04 16:36:28 +0200

    render the SPinner if the value of loading is true

--- src/components/characters/CharacterGrid.js
+++ src/components/characters/CharacterGrid.js
@@ -4,7 +4,7 @@ import Spinner from '../ui/Spinner'
 
 const CharacterGrid = ({ isLoading, items }) => {
   return isLoading ? (
-    <h1>Loading</h1>
+    <Spinner />
   ) : (
     <section className='cards'>
       {items.map((item) => (
